f6cd781a84fa478c8350fc0d38a3bbb21ef336f0
casio123 <broerja@gmail.com>
2016-08-10 22:02:08 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-	for(i = 0; i < 3 ; i++){
-		$('.container').append("<div></div>")
-	}
+    var squares = 16;
+    createGrid(squares);
+    
+    $('#clearButton').click(function(){
+        createGrid(clearGrid());
+        
+        
+    });
+     function clearGrid(){
+        squares = prompt("How many squares per side for the new grid?");
+        $('.container').empty();
+        return squares;
+        };
+    function randomColor(){
+        var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        return hue;
+    }
+    function createGrid(squares){
+        for(i = 0; i < squares; i++){
+            $('.container').append('<p />');
+		  for(j=0; j < squares; j++){
+              $('.container').append("<div class='square'></div>")}
+	       }
+        $('.square').hover(function(){
+        $(this).css('background-color',randomColor());
+    });
+    }
+
+
+
 });
+
+
+
